ee4dfa56b08b912841a02ad99fe63d3787ac994e
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-03-28 01:12:11 +0530

    Correct Data scraping !

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ def getLatestArticles():
     response = requests.get(TECHCRUNCH_URL)
     
     if response.status_code == 200:    
+
+        
         page = html.fromstring(response.content)
-        title = page.xpath("//div[@class='feature-island']/div/h2/a/text()")
-        return {"text" : title}
+        divContent = [element.text for element in page.xpath("//a[@class='post-block__title__link']")]
+        newsDictionary = {i + 1: text for i, text in enumerate(divContent)}
+        newsDictionary = {key: value.strip() for key, value in newsDictionary.items()}
+        newsDictionary = {key: value for key, value in newsDictionary.items() if value.strip() != ""}
+     
+        return newsDictionary
     else:
         return {"error" : "Failed to fetch data from Techcrunch"}
 
