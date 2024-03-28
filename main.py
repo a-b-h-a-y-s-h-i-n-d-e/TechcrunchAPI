192d7e00e74c40226c566d0131f4df4727fb590e
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-03-29 00:15:52 +0530

    first for Vercel!

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ app = FastAPI()
 TECHCRUNCH_URL = "https://techcrunch.com/"
 
 
+
+
+
 @app.get('/')
-def getLatestArticles():
+async def getLatestArticles():
     response = requests.get(TECHCRUNCH_URL)
     
     if response.status_code == 200:    
 
-        
         page = html.fromstring(response.content)
         divContent = [element.text for element in page.xpath("//a[@class='post-block__title__link']")]
+
         newsDictionary = {i + 1: text for i, text in enumerate(divContent)}
+        # removing \t\n
         newsDictionary = {key: value.strip() for key, value in newsDictionary.items()}
+        # removing empty key-value like this ""
         newsDictionary = {key: value for key, value in newsDictionary.items() if value.strip() != ""}
      
         return newsDictionary
