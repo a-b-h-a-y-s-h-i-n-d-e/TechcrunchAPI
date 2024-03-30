d712a2b570295625e8b1b7ae94c546bccb571350
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-03-30 14:19:47 +0530

    ::

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from lxml import html
 import requests
 import uvicorn
 
 app = FastAPI()
 TECHCRUNCH_URL = "https://techcrunch.com/"
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.get('/')
 async def getLatestArticles():
@@ -27,4 +45,3 @@ async def getLatestArticles():
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "0.0.0.0", port = 8000)
-
